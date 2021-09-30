c707fefcbcb63cc64320d797f71d246937599d1f
Maria Pallares <mpallareslara@gmail.com>
2021-09-30 20:56:20 +0200

    FEAT: Navigation component added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Navigation from './Components/Navigation/Navigation'
 function App() {
   return (
     <div className="App">
       <h1>hey</h1>
+      <Navigation/>
     </div>
   );
 }
---
+++ client/src/Components/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Users</li>
+          <li>Privacy</li>
+          <li>Done</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
