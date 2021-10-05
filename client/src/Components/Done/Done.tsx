43b711f02904804ca0a3fc242a5731f3a248618e
Maria Pallares <mpallareslara@gmail.com>
2021-10-05 03:25:41 +0200

    STYLE: Styled Done component

--- client/src/Components/Done/Done.tsx
+++ client/src/Components/Done/Done.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BsCheckLg } from 'react-icons/bs';
+import './Done.css';
 
-export const  Done: React.FC<{}> = ():JSX.Element =>{
-
+export const Done: React.FC<{}> = (): JSX.Element => {
   return (
-    <div>
-      <h3>doneeee</h3>
+    <div className="done-main-container">
+      <div className="done-form-container">
+        <div className="check-icon">
+          {<BsCheckLg/>}
+        </div>
+        <p className="text-done">
+          Please verify your email address, you should have received an email
+          from us already!
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
