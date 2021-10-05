f16714f7cc044fd6074a8b0325263c23c2167c59
Maria Pallares <mpallareslara@gmail.com>
2021-10-05 19:07:06 +0200

    FEAT: Console.log final result in JSON format working

--- client/src/Components/Done/Done.tsx
+++ client/src/Components/Done/Done.tsx
@@ -1,28 +1,28 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
-import { useAppDispatch } from '../../redux/hooks';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { ActionType } from '../../redux/actions/actionTypes';
 import { BsCheckLg } from 'react-icons/bs';
 import './Done.css';
 
 export const Done: React.FC<{}> = (): JSX.Element => {
+  const userStore = useAppSelector((state) => state.user);
+  const checkboxStore = useAppSelector((state) => state.checkbox);
+  const history = useHistory();
+  const dispatch = useAppDispatch();
 
-  const history = useHistory()
-  const dispatch = useAppDispatch()
-  
-
-useEffect(() => {
-  setTimeout(() => {
-    history.push('/')
-  dispatch({type: ActionType.CREATE__PAGE, payload: 'users'})
-  }, 5000)
-}, [])
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/');
+      dispatch({ type: ActionType.CREATE__PAGE, payload: 'users' });
+      console.log('USER INFO', JSON.stringify(userStore));
+      console.log('CHECKBOX INFO', JSON.stringify(checkboxStore));
+    }, 5000);
+  }, []);
   return (
     <div className="done-main-container">
       <div className="done-form-container">
-        <div className="check-icon">
-          {<BsCheckLg/>}
-        </div>
+        <div className="check-icon">{<BsCheckLg />}</div>
         <p className="text-done">
           Please verify your email address, you should have received an email
           from us already!
